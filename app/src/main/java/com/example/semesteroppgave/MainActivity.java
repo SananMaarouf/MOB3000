4231e473e6815b76c98a0df0f5f8700304a9b17e
Sanan Maarouf <sanan.yougov@gmail.com>
2020-10-20 17:46:35 +0200

    Kobling til firestore db med test data

--- app/src/main/java/com/example/semesteroppgave/MainActivity.java
+++ app/src/main/java/com/example/semesteroppgave/MainActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -22,26 +23,46 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
-    FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private static final String TAG  = "MainActivity";
 
+    private static final String KEY_TITLE = "title";
+    private static final String KEY_DESCRIPTION = "description";
+
+    private EditText editTextTitle;
+    private EditText editTextDescription;
+
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final Button btn_yes = findViewById(R.id.btn_yes);
-        btn_yes.setOnClickListener(v -> {
-            Toast toast_yes = Toast.makeText(getApplicationContext(), "It wørk", Integer.parseInt("2"));
-            toast_yes.show();
-
-        });
-        final Button btn_no = findViewById(R.id.btn_no);
-        btn_no.setOnClickListener(v -> {
-            Toast toast_no = Toast.makeText(getApplicationContext(), "It no wørk", Integer.parseInt("2"));
-            toast_no.show();
-
-
-        });
+        editTextTitle = findViewById(R.id.edit_text_title);
+        editTextDescription = findViewById(R.id.edit_text_description);
     }
+
+    public void saveNote(View v) {
+        String title = editTextTitle.getText().toString();
+        String description = editTextDescription.getText().toString();
+
+        Map<String, Object> note = new HashMap<>();
+        note.put(KEY_TITLE, title);
+        note.put(KEY_DESCRIPTION, description);
+
+        db.collection("Notebook").document("My first note").set(note)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Toast.makeText(MainActivity.this, "Note saved", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(MainActivity.this, "Error", Toast.LENGTH_SHORT).show();
+                        Log.d(TAG, e.toString());
+                    }
+                });
+        }
+
 }
