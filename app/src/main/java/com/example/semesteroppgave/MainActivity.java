0df8b1a526a81d1f841b35faa4b22ec7870d2d2d
stianflatset <stian.tonning@hotmail.no>
2020-10-20 12:15:05 +0200

    I did something

--- app/src/main/java/com/example/semesteroppgave/MainActivity.java
+++ app/src/main/java/com/example/semesteroppgave/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.semesteroppgave;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button button = findViewById(R.id.btn_yes);
+        button.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v) {
+                Toast toast_yes = Toast.makeText(getApplicationContext(), "It wørk", Integer.parseInt("5"));
+                toast_yes.show();
+
+            }
+    });
     }
 }
